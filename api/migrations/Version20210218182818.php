ea3d110bb65d8b3a0a6e0a8eb5629475c69af431
Jack <14117126+jdipper@users.noreply.github.com>
2021-02-18 21:53:16 +0000

    Rebased on to api-platform:2.6.2

--- api/migrations/Version20210218182818.php
+++ api/migrations/Version20210218182818.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210209205348 extends AbstractMigration
+final class Version20210218182818 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,15 @@ final class Version20210209205348 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
-
-        $this->addSql('CREATE SEQUENCE event_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE contact_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE "event_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE page_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE ticket_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE ticket_type_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE event (id INT NOT NULL, title VARCHAR(255) NOT NULL, date DATE NOT NULL, booking_open DATE NOT NULL, booking_close DATE NOT NULL, venue VARCHAR(255) NOT NULL, description TEXT DEFAULT NULL, princpal_speaker VARCHAR(255) DEFAULT NULL, sponsor VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE contact (id INT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, subject VARCHAR(255) NOT NULL, message TEXT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE "event" (id INT NOT NULL, title VARCHAR(255) NOT NULL, date DATE NOT NULL, booking_open DATE NOT NULL, booking_close DATE NOT NULL, venue VARCHAR(255) NOT NULL, description TEXT DEFAULT NULL, princpal_speaker VARCHAR(255) DEFAULT NULL, sponsor VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE page (id INT NOT NULL, title VARCHAR(255) NOT NULL, content_html TEXT NOT NULL, content_md TEXT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, is_published BOOLEAN NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE ticket (id INT NOT NULL, owner_id INT NOT NULL, event_id INT NOT NULL, ticket_type_id INT NOT NULL, rank VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, seating_preference TEXT NOT NULL, dietary VARCHAR(255) NOT NULL, paid BOOLEAN NOT NULL, created_date DATE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_97A0ADA37E3C61F9 ON ticket (owner_id)');
         $this->addSql('CREATE INDEX IDX_97A0ADA371F7E88B ON ticket (event_id)');
@@ -33,26 +36,34 @@ final class Version20210209205348 extends AbstractMigration
         $this->addSql('COMMENT ON COLUMN ticket.seating_preference IS \'(DC2Type:array)\'');
         $this->addSql('CREATE TABLE ticket_type (id INT NOT NULL, event_id INT NOT NULL, symposium BOOLEAN NOT NULL, dinner BOOLEAN NOT NULL, serving BOOLEAN NOT NULL, student BOOLEAN NOT NULL, guest BOOLEAN NOT NULL, created_date DATE NOT NULL, price DOUBLE PRECISION NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_BE05421171F7E88B ON ticket_type (event_id)');
+        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, phone_number VARCHAR(50) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649F85E0677 ON "user" (username)');
         $this->addSql('ALTER TABLE ticket ADD CONSTRAINT FK_97A0ADA37E3C61F9 FOREIGN KEY (owner_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE ticket ADD CONSTRAINT FK_97A0ADA371F7E88B FOREIGN KEY (event_id) REFERENCES event (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE ticket ADD CONSTRAINT FK_97A0ADA371F7E88B FOREIGN KEY (event_id) REFERENCES "event" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE ticket ADD CONSTRAINT FK_97A0ADA3C980D5C1 FOREIGN KEY (ticket_type_id) REFERENCES ticket_type (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE ticket_type ADD CONSTRAINT FK_BE05421171F7E88B FOREIGN KEY (event_id) REFERENCES event (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE ticket_type ADD CONSTRAINT FK_BE05421171F7E88B FOREIGN KEY (event_id) REFERENCES "event" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
-
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE ticket DROP CONSTRAINT FK_97A0ADA371F7E88B');
         $this->addSql('ALTER TABLE ticket_type DROP CONSTRAINT FK_BE05421171F7E88B');
         $this->addSql('ALTER TABLE ticket DROP CONSTRAINT FK_97A0ADA3C980D5C1');
-        $this->addSql('DROP SEQUENCE event_id_seq CASCADE');
+        $this->addSql('ALTER TABLE ticket DROP CONSTRAINT FK_97A0ADA37E3C61F9');
+        $this->addSql('DROP SEQUENCE contact_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE "event_id_seq" CASCADE');
+        $this->addSql('DROP SEQUENCE page_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE ticket_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE ticket_type_id_seq CASCADE');
-        $this->addSql('DROP TABLE event');
+        $this->addSql('DROP SEQUENCE "user_id_seq" CASCADE');
+        $this->addSql('DROP TABLE contact');
+        $this->addSql('DROP TABLE "event"');
+        $this->addSql('DROP TABLE page');
         $this->addSql('DROP TABLE ticket');
         $this->addSql('DROP TABLE ticket_type');
+        $this->addSql('DROP TABLE "user"');
     }
 }
