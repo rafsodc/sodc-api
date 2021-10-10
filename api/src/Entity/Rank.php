0b5db452b3a45848d27634ff61bfe4bd50c480e9
Jack <14117126+jdipper@users.noreply.github.com>
2021-10-10 15:45:53 +0000

    Fix user lookup query. Return rank on user collect

--- api/src/Entity/Rank.php
+++ api/src/Entity/Rank.php
@@ -39,7 +39,7 @@ class Rank
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"rank:read"})
+     * @Groups({"rank:read", "user:collection:read"})
      */
     private $rank;
 
