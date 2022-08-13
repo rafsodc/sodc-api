a452578b09238485a3bbec5c3ddf15cf7d3f7376
Jack <14117126+jdipper@users.noreply.github.com>
2022-08-13 17:37:17 +0100

    Changes to the healthcheck

--- api/docker/transport/docker-entrypoint.sh
+++ api/docker/transport/docker-entrypoint.sh
@@ -1,3 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/sh
+set -e
+
 sleep 10;
 /srv/api/bin/console messenger:consume email_priority_high email_priority_normal email_priority_low >&1;
---
+++ api/docker/transport/docker-healthcheck.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+PROCESS="php /srv/api/bin/console messenger:consume"
+
+restart_container() {
+    echo "Stopping workers"
+    php /srv/api/bin/console messenger:stop-workers
+    exit 1
+}
+
+restart_at_random() {
+    # 1/90 chance of reset, based on 6 attemps over 15 minutes
+    limit=$(shuf -i 0-89 -n 1)
+    if [ $limit = 0 ]; then restart_container; fi
+}
+
+check_memory() {
+    case 1:${1:--} in  
+        # If it does not end in an m, and is not a number, restart the container.
+        (1:*[!m]|1:*[!0-9]*[m]) restart_container ;;
+        # If member is above 128 restart container
+        ($((${1%?}>128))*) restart_container ;;
+    esac
+}
+
+
+# Check if the process exists
+if output=$(docker-compose --compatibility exec transport ps -o etime,vsz,args | grep "$PROCESS"); then
+    # Get the time in minutes
+    mem=$(echo $output | cut -d' ' -f2 )
+    time="46"
+    case 1:${time:--}  in
+        # If the time contains a 'd' or 'h', it will not be a number, so restart if it's not a number
+        (1:*[!0-9]*) restart_container ;;
+        # If the time is >= 45, we will restart the container randomly (to avoid both containers restarting at the same time) 
+        ($((time>=45))*) restart_at_random ;;
+        # Otherwise, check memory usage
+        *) check_memory mem;;
+    esac
+else
+    restart_container
+fi
+
+exit 0
