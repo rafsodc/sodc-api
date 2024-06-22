b747ce71f36b5804e962ad05097354922044aa02
Jack <14117126+jdipper@users.noreply.github.com>
2024-06-22 14:33:53 +0100

    Minor error fix on migration

--- api/migrations/Version20240622122328.php
+++ api/migrations/Version20240622122328.php
@@ -37,7 +37,7 @@ final class Version20240622122328 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
+        //$this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP TABLE messenger_messages');
     }
 }
